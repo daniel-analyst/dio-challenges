da74723a66d1314ee551dd44f7677638b7d97e8d
Daniel Silva Marcelino <danielsystems.analyst@gmail.com>
2024-08-20 10:31:19 -0300

    feat: creating a small system for validation of selection process

---
+++ formacao-java-developer/criando-um-pequeno-sistema-para-validacao-de-processo-seletivo/src/main/java/com/daniel/java/basico/Contador.java
@@ -0,0 +1,35 @@
+package com.daniel.java.basico;
+
+import com.daniel.java.basico.exceptions.ParametrosInvalidosException;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt();
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt();
+
+        try {
+            contar(parametroUm, parametroDois);
+
+        }catch (ParametrosInvalidosException parametrosInvalidosException) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+    }
+
+    public static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        if (parametroUm > parametroDois) {
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " +  i);
+        }
+    }
+}
