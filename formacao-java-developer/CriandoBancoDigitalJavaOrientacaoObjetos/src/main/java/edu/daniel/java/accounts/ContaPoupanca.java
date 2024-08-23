7d2eff8bc754b5702110d13c287b2f2684f34fd6
Daniel Silva Marcelino <danielsystems.analyst@gmail.com>
2024-08-22 23:33:33 -0300

    feat: Creating a Digital Bank with Java and Object Orientation. Lombok was included.

---
+++ formacao-java-developer/CriandoBancoDigitalJavaOrientacaoObjetos/src/main/java/edu/daniel/java/accounts/ContaPoupanca.java
@@ -0,0 +1,17 @@
+package edu.daniel.java.accounts;
+
+import edu.daniel.java.abstracts.Conta;
+import edu.daniel.java.models.Cliente;
+
+public class ContaPoupanca extends Conta {
+
+    public ContaPoupanca(Cliente cliente) {
+        super(cliente);
+    }
+
+    @Override
+    public void imprimirExtrato() {
+        System.out.println("=== Extrato Conta Poupança ===");
+        super.imprimirInfosComuns();
+    }
+}
---
+++ formacao-java-developer/CriandoBancoDigitalJavaOrientacaoObjetos/src/main/java/edu/daniel/java/models/Cliente.java
@@ -0,0 +1,12 @@
+package edu.daniel.java.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Cliente {
+
+    private String nome;
+
+}
